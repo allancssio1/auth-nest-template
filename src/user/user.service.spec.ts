e5983d4e16a2fb7fe40b37ef9290763bde88ddf3
Allan Cassio <allan.cassio1@gmail.com>
2024-01-04 17:28:04 -0300

    update readme

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,8 +1,7 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './user.service';
-import { randomUUID } from 'crypto';
-import { PrismaService } from '../prisma/prisma.service';
-import { User } from './entities/user.entity';
+import { Test, TestingModule } from '@nestjs/testing'
+import { UserService } from './user.service'
+import { randomUUID } from 'crypto'
+import { User } from './entities/user.entity'
 
 const users: User[] = [
   {
@@ -10,8 +9,6 @@ const users: User[] = [
     name: 'mock-name-1',
     email: 'mock-email-1@email.com',
     password: 'password-mock-1' || null,
-    bio: 'bio-mock-1' || null,
-    birthDate: new Date('1991-01-26T22:23:04.209Z') || null,
     created_at: new Date(),
   },
   {
@@ -19,8 +16,6 @@ const users: User[] = [
     name: 'mock-name-2',
     email: 'mock-email-2@email.com',
     password: 'password-mock-2' || null,
-    bio: 'bio-mock-2' || null,
-    birthDate: new Date('1991-01-26T22:23:04.209Z') || null,
     created_at: new Date(),
   },
   {
@@ -28,11 +23,9 @@ const users: User[] = [
     name: 'mock-name-3',
     email: 'mock-email-3@email.com',
     password: 'password-mock-3' || null,
-    bio: 'bio-mock-3' || null,
-    birthDate: new Date('1991-01-26T22:23:04.209Z') || null,
     created_at: new Date(),
   },
-];
+]
 
 const db = {
   user: {
@@ -47,51 +40,54 @@ const db = {
     }),
     delete: jest.fn().mockResolvedValue('User Deleted'),
   },
-};
+}
 
 describe('Testing UserService', () => {
-  let service: UserService;
+  let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, { provide: PrismaService, useValue: db }],
-    }).compile();
+      providers: [
+        UserService,
+        { provide: `ORM or Database Service`, useValue: db },
+      ],
+    }).compile()
 
-    service = module.get<UserService>(UserService);
-  });
+    service = module.get<UserService>(UserService)
+  })
 
   it('should be defined', async () => {
-    expect(service).toBeDefined();
-  });
+    expect(service).toBeDefined()
+  })
 
   it('Should be get user by email', async () => {
-    const user = await service.findByEmail('mock-email@email.com');
+    const user = await service.findByEmail('mock-email@email.com')
     expect(user).toEqual(
       expect.objectContaining({
         email: 'mock-email-1@email.com',
       }),
-    );
-  });
+    )
+  })
 
   it('Should be create user', async () => {
     const user = await service.create({
       name: 'mock-name-1',
       email: 'mock-emai1-1@email.com',
       password: 'passwor-mock',
-    });
+    })
 
-    expect(user.id).toEqual(expect.any(String));
+    expect(user.id).toEqual(expect.any(String))
     expect(user).toEqual(
       expect.objectContaining({
         name: 'mock-name-1',
         email: 'mock-email-1@email.com',
         password: null,
       }),
-    );
-  });
+    )
+  })
 
   it('Should be get all users', async () => {
-    const usersArray = await service.findAll();
+    const usersArray = await service.findAll()
 
     expect(usersArray).toEqual([
       expect.objectContaining({
@@ -106,36 +102,34 @@ describe('Testing UserService', () => {
         name: 'mock-name-3',
         email: 'mock-email-3@email.com',
       }),
-    ]);
-  });
+    ])
+  })
 
   it('Should be get user by id', async () => {
-    const user = await service.findUserById(users[0].id);
+    const user = await service.findUserById(users[0].id)
 
     expect(user).toEqual(
       expect.objectContaining({
         name: 'mock-name-1',
       }),
-    );
-  });
+    )
+  })
 
   it('Should be update user', async () => {
     const user = await service.update(users[0].id, {
       name: 'mock-name-4',
-      bio: 'bio-mock-4',
-    });
+    })
 
     expect(user).toEqual(
       expect.objectContaining({
         name: 'mock-name-4',
-        bio: 'bio-mock-4',
       }),
-    );
-  });
+    )
+  })
 
   it('Should be possible delete user', async () => {
-    const response = await service.remove(users[0].id);
+    const response = await service.remove(users[0].id)
 
-    expect(response).toEqual('User Deleted');
-  });
-});
+    expect(response).toEqual('User Deleted')
+  })
+})
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { hash } from 'bcrypt';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { hash } from 'bcrypt'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
 export class UserService {
@@ -11,60 +11,45 @@ export class UserService {
     const data = {
       ...createUserDto,
       password: await hash(createUserDto.password, 6),
-    };
+    }
 
-    const user = null;
-    // await this.repositorio.user.create({
-    //   data,
-    // });
+    const user = null
 
     return {
       ...user,
       password: null,
-    };
+    }
   }
 
   async findByEmail(email: string) {
-    const user = null;
-    // await this.repositorio.user.findUnique({ where: { email } });
-    return user || null;
+    const user = null
+
+    return user || null
   }
 
   async findUserById(id: string) {
-    const user = null;
-    // await this.repositorio.user.findUnique({ where: { id } });
-    return user ?? null;
+    const user = null
+
+    return user ?? null
   }
 
   async findAll() {
-    const user = null;
-    // await this.repositorio.user.findMany();
-    return user ?? null;
+    const user = null
+
+    return user ?? null
   }
 
   async update(id: string, data: UpdateUserDto) {
-    const user = null;
-    // await this.repositorio.user.findUnique({ where: { id } });
+    const user = null
 
-    if (!user) throw new Error('User not found!');
+    if (!user) throw new Error('User not found!')
 
-    const userUpdated = null;
-    // await this.repositorio.user.update({
-    //   where: { id },
-    //   data,
-    // });
+    const userUpdated = null
 
-    return userUpdated ?? null;
+    return userUpdated ?? null
   }
 
   async remove(id: string) {
-    const user = null;
-    // await this.repositorio.user.findUnique({ where: { id } });
-
-    if (!user) throw new Error('User not found!');
-
-    // await this null.repositorio.user.delete({ where: { id } });
-
-    return 'User Deleted';
+    return 'User Deleted'
   }
 }
